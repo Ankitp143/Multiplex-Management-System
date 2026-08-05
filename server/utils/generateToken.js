03898f7b2506416aa4633bbc577269034d7c4928
UP-73 <ap8330231@gmail.com>
2026-08-06 02:20:09 +0530

    Add JWT token generation utility

---
+++ server/utils/generateToken.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+/**
+ * Generate JWT Token
+ * @param {Object} user
+ * @returns {String}
+ */
+const generateToken = (user) => {
+    return jwt.sign(
+        {
+            id: user._id,
+            role: user.role,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d",
+        }
+    );
+};
+
+module.exports = generateToken;
